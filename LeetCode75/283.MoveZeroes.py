8bdfacc934d977b7a4e7641081f8a01f44e2969c
aprilllll <51517439+aprilllllllll@users.noreply.github.com>
2025-03-05 23:47:20 -0500

    Update 283.MoveZeroes.py

--- LeetCode75/283.MoveZeroes.py
+++ LeetCode75/283.MoveZeroes.py
@@ -1,10 +1,5 @@
 class Solution(object):
     def moveZeroes(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: None Do not return anything, modify nums in-place instead.
-        """
-
 
         for x in nums:
             if x == 0:
