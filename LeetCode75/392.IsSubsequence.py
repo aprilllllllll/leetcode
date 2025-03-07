b862949c5e1b742d675a1ef7234a65c1cdbee54b
wingtunglau <wtlau@mysenca.ca>
2025-03-06 23:50:54 -0500

    add 392

---
+++ LeetCode75/392.IsSubsequence.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def isSubsequence(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s) > len(t):return False
+        if len(s) == 0:return True
+
+        subsequence=0
+        for i in range(0,len(t)):
+            print(subsequence)
+            if subsequence < len(s):
+                print(s[subsequence])
+                if s[subsequence]==t[i]:
+
+                    subsequence+=1
+        return  subsequence == len(s)         
+
+s = "abc"
+t = "ahbgdc"
+# s = "axc" 
+# t = "ahbgdc"
+solution = Solution()
+print(solution.isSubsequence(s,t))
