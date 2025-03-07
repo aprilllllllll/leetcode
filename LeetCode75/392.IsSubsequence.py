25f370f6fd1a0cb559fd1201899a1fccf4f64b93
wingtunglau <wtlau@mysenca.ca>
2025-03-06 23:57:17 -0500

    update 392

--- LeetCode75/392.IsSubsequence.py
+++ LeetCode75/392.IsSubsequence.py
@@ -8,15 +8,13 @@ class Solution(object):
         if len(s) > len(t):return False
         if len(s) == 0:return True
 
-        subsequence=0
-        for i in range(0,len(t)):
-            print(subsequence)
-            if subsequence < len(s):
-                print(s[subsequence])
-                if s[subsequence]==t[i]:
-
-                    subsequence+=1
-        return  subsequence == len(s)         
+        p=0
+        for x in t:
+            if s[p]== x:
+                p+=1
+            if p == len(s):
+                return True
+        return False
 
 s = "abc"
 t = "ahbgdc"
