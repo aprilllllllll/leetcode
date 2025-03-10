917db8e4e16145fd8d2445bda984240a0bfc086b
wingtunglau <wtlau@mysenca.ca>
2025-03-10 00:19:08 -0400

    add 334

---
+++ LeetCode75/334.IncreasingTripletSubsequence.py
@@ -0,0 +1,22 @@
+
+class Solution(object):
+    def increasingTriplet(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        first = float('inf')
+        second = float('inf')
+        for x in nums:
+            if x<= first:
+                first = x
+            elif x<= second:
+                second = x
+            else:
+                return True
+
+        return False
+
+solution = Solution()
+nums = [2,1,5,0,4,6]
+print(solution.increasingTriplet(nums))
