b2700a0b47c2037f88d00ad194a0af7d7e137130
wingtunglau <wtlau@mysenca.ca>
2025-03-17 23:29:52 -0400

    add 643

---
+++ LeetCode75/643.MaximumAverage.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def findMaxAverage(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: float
+        """
+        if k == 1: return max(nums)
+
+        max_sum = temp = sum(nums[:k])
+
+        for x in range(k,len(nums)):
+            temp = temp -nums[x-k]+nums[x]
+            if temp > max_sum:
+                max_sum = temp
+
+        return float(max_sum)/k
