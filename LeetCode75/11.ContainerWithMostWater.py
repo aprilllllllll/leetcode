ce3884cd6642a5edfc9983fa41cf25322e236744
wingtunglau <wtlau@mysenca.ca>
2025-03-27 21:00:23 -0400

    add 11

---
+++ LeetCode75/11.ContainerWithMostWater.py
@@ -0,0 +1,38 @@
+class Solution(object):
+    def maxArea(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        
+        maxArea =0 
+        area = 0
+        
+        left = 0
+        right = len(height)-1
+
+        while left<right:
+            if height[left]> height[right]:
+                h = height[right]
+            else:
+                h = height[left]
+
+            w = right -left
+
+            maxArea = max(maxArea,h*w)
+
+            if height[left]> height[right]:
+                right -=1
+            else:
+                left +=1
+
+
+   
+
+        return maxArea
+
+
+solution = Solution()
+height =[1,8,6,2,5,4,8,3,7]
+
+print(solution.maxArea(height))
