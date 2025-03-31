ce9b6a297ed9c588f137e175696b772f407bf8f9
wingtunglau <wtlau@mysenca.ca>
2025-03-30 21:39:06 -0400

    add 53

---
+++ LeetCode75/53.maximumSubarray.py
@@ -0,0 +1,34 @@
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if len(nums) == 1:
+            return nums[0]
+
+
+        maxSum = nums[0]
+        currSum =0
+
+
+
+        for n in nums:
+            c = c+n
+            maxSum = max(maxSum,c)
+            if c<0:
+                c=0
+
+
+
+
+        return maxSum
+
+
+
+
+solution = Solution()
+
+nums = [-2,-1,-3,-4,-1,-2,-1,-5,-4]
+
+print(solution.maxSubArray(nums))
