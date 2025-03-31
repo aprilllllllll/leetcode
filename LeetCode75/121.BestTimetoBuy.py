86b5d438cfe2359aed18692e00bbbc758231ebba
wingtunglau <wtlau@mysenca.ca>
2025-03-30 21:08:04 -0400

    121

---
+++ LeetCode75/121.BestTimetoBuy.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        maxProfit = 0
+        lowprice =float('inf')
+
+        for p in prices:
+
+            lowprice = min(lowprice,p)
+
+
+            maxProfit = max(maxProfit, p-lowprice)
+        
+        return maxProfit
+
+solution = Solution()
+prices =[2,4,1]
+
+print(solution.maxProfit(prices))
