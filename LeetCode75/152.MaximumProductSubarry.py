1e578fe30cadb844fd0e23154fe69ab3b3904012
wingtunglau <wtlau@mysenca.ca>
2025-03-31 21:12:54 -0400

    add 152

---
+++ LeetCode75/152.MaximumProductSubarry.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def maxProduct(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        max_product = 1
+        min_product = 1
+        result = float('-inf')
+        for n in nums:
+            curr = n
+
+            if curr< 0:
+                max_product,min_product= min_product,max_product
+
+            
+            max_product = max(max_product*curr,curr)
+            min_product = min(min_product*curr,curr)
+            result = max(result,max_product)
+        
+        return result
+
+solution = Solution()
+nums = [3,-1,4]
+print(solution.maxProduct(nums))
---
+++ LeetCode75/217.containsDuplicate.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        l = list(set(nums))
+        if len(l) == len(nums):
+            return False
+
+        else:
+            return True
