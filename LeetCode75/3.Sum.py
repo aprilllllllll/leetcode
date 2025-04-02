2ace4925feecb8e786c7e3fe7905c905e7f8eefa
wingtunglau <wtlau@mysenca.ca>
2025-04-01 22:15:19 -0400

    3

---
+++ LeetCode75/3.Sum.py
@@ -0,0 +1,39 @@
+
+
+
+from operator import le
+
+
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        res = []
+        nums.sort()
+        for i in range(len(nums)):
+            if i>0 and nums[i]== nums[i-1]:
+                continue
+            left = i+1
+            right = len(nums)-1
+            while left <right:
+                
+
+                sum = i + nums[left] + nums[right]
+                if sum >0:
+                    right-=1
+                elif sum<0:
+                    left+=1
+                else:
+                    res.append([nums,nums[left],nums[right]])
+                    left+=1
+                
+
+
+        return res
+
+
+solution = Solution()
+nums = [-1,0,1,2,-1,-4]
+print(solution.threeSum(nums))
---
+++ LeetCode75/33.SearchInRotatedSortedArray.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+    
+        if target not in nums:
+            return -1
+
+        else:
+            
+            return nums.index(target)
+
+solution = Solution()
+nums = [4,5,6,7,0,1,2]
+target = 0
+print(solution.search(nums,target))
