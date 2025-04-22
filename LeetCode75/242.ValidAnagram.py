d3dba9a071cec2b6a9a0c5321c4433e2d166502a
wingtunglau <wtlau@mysenca.ca>
2025-04-22 07:18:51 -0400

    add 242

---
+++ LeetCode75/242.ValidAnagram.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s)!=len(t):
+            return False
+        else:
+
+            for i in set(s):
+                if s.count(i)!=t.count(i):
+                    return False
+            return True
+solution = Solution()
+s="rat"
+t="car"
+print(solution.isAnagram(s,t))
