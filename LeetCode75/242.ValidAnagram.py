7ac71499456af9e181831084c5e1620da34561bf
aprilllll <51517439+aprilllllllll@users.noreply.github.com>
2025-04-23 23:53:03 -0400

    Update 242.ValidAnagram.py

--- LeetCode75/242.ValidAnagram.py
+++ LeetCode75/242.ValidAnagram.py
@@ -8,7 +8,6 @@ class Solution(object):
         if len(s)!=len(t):
             return False
         else:
-
             for i in set(s):
                 if s.count(i)!=t.count(i):
                     return False
